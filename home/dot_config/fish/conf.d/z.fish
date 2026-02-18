f4c17ed6e26e55e2a9d33b264cd4ac793011b714
Michał Krowiarz <m.krowiarz@gmail.com>
2026-02-19 00:16:31 +0100

    zoxide

--- home/dot_config/fish/conf.d/z.fish
+++ home/dot_config/fish/conf.d/z.fish
@@ -1,63 +1,106 @@
-if test -z "$Z_DATA"
-    if test -z "$XDG_DATA_HOME"
-        set -U Z_DATA_DIR "$HOME/.local/share/z"
+# =============================================================================
+#
+# Utility functions for zoxide.
+#
+
+# pwd based on the value of _ZO_RESOLVE_SYMLINKS.
+function __zoxide_pwd
+    builtin pwd -L
+end
+
+# A copy of fish's internal cd function. This makes it possible to use
+# `alias cd=z` without causing an infinite loop.
+if ! builtin functions --query __zoxide_cd_internal
+    if status list-files functions/cd.fish &>/dev/null
+        status get-file functions/cd.fish | string replace --regex -- '^function cd\s' 'function __zoxide_cd_internal ' | source
     else
-        set -U Z_DATA_DIR "$XDG_DATA_HOME/z"
-    end
-    set -U Z_DATA "$Z_DATA_DIR/data"
-end
-
-if test ! -e "$Z_DATA"
-    if test ! -e "$Z_DATA_DIR"
-        mkdir -p -m 700 "$Z_DATA_DIR"
-    end
-    touch "$Z_DATA"
-end
-
-if test -z "$Z_CMD"
-    set -U Z_CMD z
-end
-
-set -U ZO_CMD "$Z_CMD"o
-
-if test ! -z $Z_CMD
-    function $Z_CMD -d "jump around"
-        __z $argv
+        string replace --regex -- '^function cd\s' 'function __zoxide_cd_internal ' <$__fish_data_dir/functions/cd.fish | source
     end
 end
 
-if test ! -z $ZO_CMD
-    function $ZO_CMD -d "open target dir"
-        __z -d $argv
+# cd + custom logic based on the value of _ZO_ECHO.
+function __zoxide_cd
+    if set -q __zoxide_loop
+        builtin echo "zoxide: infinite loop detected"
+        builtin echo "Avoid aliasing `cd` to `z` directly, use `zoxide init --cmd=cd fish` instead"
+        return 1
+    end
+    __zoxide_loop=1 __zoxide_cd_internal $argv
+end
+
+# =============================================================================
+#
+# Hook configuration for zoxide.
+#
+
+# Initialize hook to add new entries to the database.
+function __zoxide_hook --on-variable PWD
+    test -z "$fish_private_mode"
+    and command zoxide add -- (__zoxide_pwd)
+end
+
+# =============================================================================
+#
+# When using zoxide with --no-cmd, alias these internal functions as desired.
+#
+
+# Jump to a directory using only keywords.
+function __zoxide_z
+    set -l argc (builtin count $argv)
+    if test $argc -eq 0
+        __zoxide_cd $HOME
+    else if test "$argv" = -
+        __zoxide_cd -
+    else if test $argc -eq 1 -a -d $argv[1]
+        __zoxide_cd $argv[1]
+    else if test $argc -eq 2 -a $argv[1] = --
+        __zoxide_cd -- $argv[2]
+    else
+        set -l result (command zoxide query --exclude (__zoxide_pwd) -- $argv)
+        and __zoxide_cd $result
     end
 end
 
-if not set -q Z_EXCLUDE
-    set -U Z_EXCLUDE "^$HOME\$"
-else if contains $HOME $Z_EXCLUDE
-    # Workaround: migrate old default values to a regex (see #90).
-    set Z_EXCLUDE (string replace -r -- "^$HOME\$" '^'$HOME'$$' $Z_EXCLUDE)
-end
+# Completions.
+function __zoxide_z_complete
+    set -l tokens (builtin commandline --current-process --tokenize)
+    set -l curr_tokens (builtin commandline --cut-at-cursor --current-process --tokenize)
 
-# Setup completions once first
-__z_complete
-
-function __z_on_variable_pwd --on-variable PWD
-    __z_add
-end
-
-function __z_uninstall --on-event z_uninstall
-    functions -e __z_on_variable_pwd
-    functions -e $Z_CMD
-    functions -e $ZO_CMD
-
-    if test ! -z "$Z_DATA"
-        printf "To completely erase z's data, remove:\n" >/dev/stderr
-        printf "%s\n" "$Z_DATA" >/dev/stderr
+    if test (builtin count $tokens) -le 2 -a (builtin count $curr_tokens) -eq 1
+        # If there are < 2 arguments, use `cd` completions.
+        complete --do-complete "'' "(builtin commandline --cut-at-cursor --current-token) | string match --regex -- '.*/$'
+    else if test (builtin count $tokens) -eq (builtin count $curr_tokens)
+        # If the last argument is empty, use interactive selection.
+        set -l query $tokens[2..-1]
+        set -l result (command zoxide query --exclude (__zoxide_pwd) --interactive -- $query)
+        and __zoxide_cd $result
+        and builtin commandline --function cancel-commandline repaint
     end
-
-    set -e Z_CMD
-    set -e ZO_CMD
-    set -e Z_DATA
-    set -e Z_EXCLUDE
 end
+complete --command __zoxide_z --no-files --arguments '(__zoxide_z_complete)'
+
+# Jump to a directory using interactive search.
+function __zoxide_zi
+    set -l result (command zoxide query --interactive -- $argv)
+    and __zoxide_cd $result
+end
+
+# =============================================================================
+#
+# Commands for zoxide. Disable these using --no-cmd.
+#
+
+abbr --erase z &>/dev/null
+complete --erase --command z
+alias z=__zoxide_z
+
+abbr --erase zi &>/dev/null
+complete --erase --command zi
+alias zi=__zoxide_zi
+
+# =============================================================================
+#
+# To initialize zoxide, add this to your configuration (usually
+# ~/.config/fish/config.fish):
+#
+#   zoxide init fish | source
